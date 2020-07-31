c6d88318f9cf824ceb23e502916df2f9687f1495
Thiago Oliveira <omugruo@gmail.com>
2020-07-31 15:13:36 -0300

    Updates and changes

--- proactive_check.py
+++ proactive_check.py
@@ -2,14 +2,73 @@
 # coding: utf-8
 
 import json, re, requests, sys, os, getopt
+import vercheck as vercheck
+
+# static product list (taken from RMT and other sources)
+product_list = {
+    1117: { 'name': 'SUSE Linux Enterprise Server 12 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12' },
+    1118: { 'name': 'SUSE Linux Enterprise Desktop 12 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12' },
+    1322: { 'name': 'SUSE Linux Enterprise Server 12 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp1' },
+    1333: { 'name': 'SUSE Linux Enterprise Desktop 12 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp1' },
+    1357: { 'name': 'SUSE Linux Enterprise Server 12 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp2' },
+    1358: { 'name': 'SUSE Linux Enterprise Desktop 12 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp2' },
+    1421: { 'name': 'SUSE Linux Enterprise Server 12 SP3 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp3' },
+    1425: { 'name': 'SUSE Linux Enterprise Desktop 12 SP3 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp3' },
+    1625: { 'name': 'SUSE Linux Enterprise Server 12 SP4 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp4' },
+    1629: { 'name': 'SUSE Linux Enterprise Desktop 12 SP4 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp4' },
+    1878: { 'name': 'SUSE Linux Enterprise Server 12 SP5 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp5' },
+    1319: { 'name': 'SUSE Linux Enterprise Server for SAP 12 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12' },
+    1346: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp1' },
+    1414: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp2' },
+    1426: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP3 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp3' },
+    1755: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP4 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp4' },
+    1880: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP5 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp5' },
+    1575: { 'name': 'SUSE Linux Enterprise Server 15 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:15' },
+    1609: { 'name': 'SUSE Linux Enterprise Desktop 15 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:15' },
+    1763: { 'name': 'SUSE Linux Enterprise Server 15 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:15:sp1' },
+    1764: { 'name': 'SUSE Linux Enterprise Desktop 15 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:15:sp1' },
+    1939: { 'name': 'SUSE Linux Enterprise Server 15 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:15:sp2' },
+    1935: { 'name': 'SUSE Linux Enterprise Desktop 15 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:15:sp2' },
+    1612: { 'name': 'SUSE Linux Enterprise Server for SAP 15 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:15' },
+    1766: { 'name': 'SUSE Linux Enterprise Server for SAP 15 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:15:sp1' },
+    1941: { 'name': 'SUSE Linux Enterprise Server for SAP 15 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:15:sp2' },
+}
+
+def check_SLESversion():
+    # Collect CPE_NAME and matches on product_list
+    #CPE_NAME="cpe:/o:suse:sles_sap:12:sp4"
+
+    regex = r"CPE_NAME=\"(.*)\""
+    try:
+        f = open('./basic-environment.txt', 'r')
+        data = f.read()
+        f.close()
+
+        matches = re.search(regex, data)
+        for p in product_list:
+            if matches.group(1) == product_list[p]['identifier']:
+                return p
+    except Exception as e:
+        print('error: {}'.format(str(e)))
+    return -1
 
 def print_banner():
-    print("=============================================================================")
-    print("                     Proactive_Check Utility - Supportconfig                 ")
-    print("                          Script Version: (beta)                             ")
-    print("                          Script Date: 2020-07-19                            ")
-    print("=============================================================================")
-    print("\n")
+    """
+    ================================================================================
+                    Proactive_Check Utility - Supportconfig
+                             Script Version: (version)
+                            Script Date: [YYYY-MM-DD]
+    ================================================================================
+    """
+    script = 'Proactive_Check Utility - Supportconfig'
+    version = 'Script Version: (beta)'
+    date = 'Script Date: 2020-07-19'
+
+    print('{:=>80}'.format('='))
+    print('{:^80}'.format(script))
+    print('{:^80}'.format(version))
+    print('{:^80}'.format(date))
+    print('{:=>80}\n'.format('='))
 
 # Get full command-line arguments
 full_cmd_arguments = sys.argv
@@ -18,7 +77,7 @@ full_cmd_arguments = sys.argv
 argument_list = full_cmd_arguments[1:]
 
 def extractSupportconfig():
-    tarCommand = "tar xvJf /home/toliveira/github/proactive_check/supportconfigs/nts_linux-ivcd_200718_2314.txz"
+    tarCommand = 'tar xvJf /home/toliveira/github/proactive_check/supportconfigs/nts_linux-ivcd_200718_2314.txz'
     os.system(tarCommand)
 
 def updateKernelBaseLine():
@@ -59,90 +118,163 @@ def updateKernelBaseLine():
 #    print(json.dumps(prods, sort_keys=False, indent=4))
     return prods
 
-def grabSupportconfig():
-    # Collecting and parsing information from supportconfig txt files via def basic_environment
-    #
+def crawlingSupportconfig():
+    # Collect and parse information from supportconfig txt files
+
     systemInformation = basic_environment()
-#    print(json.dumps(systemInformation, sort_keys=False, indent=4))
+    #Debug
+    #print(json.dumps(systemInformation, sort_keys=False, indent=4))
     return systemInformation
 
 def basic_environment():
-    with open("basic-environment.txt_sap") as f:
+    #Collect useful information from basic_environment.txt
+
+    with open('./basic-environment.txt_sap') as f:
         systemInformation = {}
         data = f.read()
         hostname = re.search('(Linux\s)([\D*]*)(.*)', data).group(2)
         uname = re.search('(Linux\s)([\D*]*)(.*)', data).group(3)
-        kernelVersion = re.search("\d.*\.\d.*\d.*\-\d.*\.\d...", data).group().replace("-d", ".1")
+
+        # Check SLES version based on product_list (hardcoded)
+        prod_id = check_SLESversion()
+        slesVersion = product_list[prod_id]['name']
+
+        # kernelVersion = re.search('\d.*\.\d.*\d.*\-\d.*\.\d...', data).group().replace('-d', '.1')
 
         # I need do discovery the properly regex for kernelVersion, I tried https://regex101.com/r/osKVNU/1 and group(3) and no donuts
-        #kernelVersion = re.search('(^Linux\s)([\D*]+)([\d\.\-\.\w]+)', data).group(3)
+        kernelVersion = re.search('(Linux\s)([\D]+)([\.\-\w]+)', data).group(3)
 
-        slesVersion = re.search("VERSION = ([\d]+)", data).group(1)
-        patchLevel = re.search("PATCHLEVEL = ([\d]+)", data).group(1)
-        #dataKernel = updateKernelBaseLine()
+        patchLevel = re.search('PATCHLEVEL = ([\d]+)', data).group(1)
+
+        #Debug
         #print(json.dumps(dataKernel, sort_keys=False, indent=4))
-        systemInformation["basic_environment"] = {
+
+        systemInformation['basic_environment'] = {
             'hostname': hostname,
             'uname': uname,
             'slesVersion': slesVersion,
             'patchLevel': patchLevel,
-            'kernelVersion': kernelVersion,
-#            'dataKernel': dataKernel}
+            'kernelVersion': kernelVersion
         }
-        isSAP = re.findall("sles_sap", data)
-        if len(isSAP) > 0:
-            systemInformation["basic_environment"].update({'isSAP': 'SLES 4 SAP'})
+
     return systemInformation
 
 def print_formats():
-        systemInformation = grabSupportconfig()
+    systemInformation = crawlingSupportconfig()
+
+    #Run the script and prints on <STDOUT> collored
+    def sto_print():
+
+        #Defining collors to use for print format
+        TGREEN = '\033[32m'  # Green Text
+        ENDC = '\033[m'      # Reset to Defaults
+        TRED = '\033[31m'    # Red Text
+
+        # Only for test purpose, needed to code test condition on crawlingSupportconfig()
+        isSupported = False
+
         print_banner()
-#Mask
-#       print("" + systemInformation["basic_environment"].get(""))
-        print("Hostname: " + systemInformation["basic_environment"].get("hostname"))
-        if "isSAP" in systemInformation["basic_environment"].keys():
-            print("SLES Version: " + systemInformation["basic_environment"].get("slesVersion") + "-SP" + systemInformation["basic_environment"].get("patchLevel") + " [" + systemInformation["basic_environment"].get("isSAP") + "]")
-        else:
-             print("SLES Version: " + systemInformation["basic_environment"].get("slesVersion") + "-SP" + systemInformation["basic_environment"].get("patchLevel"))
-        print("Installed Kernel: " + systemInformation["basic_environment"].get("kernelVersion"))
+        """
+        # print_banner()
+        ========================================================================
+                 Proactive_Check Utility - Supportconfig                     
+                          Script Version: (beta)                             
+                         Script Date: 2020-07-19                             
+        ========================================================================
+        #<header>
+        [SLES Version]
+        SUSE Linux Enterprise Server 12 SP4 x86_64             [ NOT SUPPORTED ]                                                               
+        #</header>______________________________________________________________
+        """
+        if isSupported == False:
+
+            #Searching for latest kernel version on https://scc.suse.com/api/package_search/packages?product_id={}
+
+            prod_id = check_SLESversion()
+            kernelVersion = str(systemInformation['basic_environment'].get('kernelVersion'))
+            latestkenel = str(vercheck.latestkKernel(prod_id))
+            print('\nServer Information:\n{:=>19}\n'.format('='))
+            print('[HOSTNAME]: {0:}\n'.format(systemInformation['basic_environment'].get('hostname')))
+            # <header>
+
+            #Mask of the print
+            #{1:} = [SLES Version]
+            #{0:<62} = SUSE Linux Enterprise Server 12 SP4 x86_64
+            #{4} = TRED
+            #{2:<80} = [NOT SUPPORTED]
+            #{5} = EDNC
+            #{3:_>80} = </header>
+            print('{1:}\n{0:<62}{4}{2:<80}\n{5}{3:_>80}\n'.format(systemInformation['basic_environment'].get('slesVersion'), '[SLES Version]', '[ OUT OF SUPPORT ]', '_', TRED, ENDC))
+            # </header>
+
+            #Debug
+            #print('kv'.format(kernelVersion))
+            #print(type(kernelVersion))
+            #print('lk'.format(latestkenel))
+            #print(type(latestkenel))
+
+            #Needed to change the logic, just for test purpose
+            if latestkenel != kernelVersion:
+                print('\n[KERNEL TEST]')
+
+                # Mask of the print
+                # {3:=>80} = '.'
+                # {2:.<49} = '[ FAILED ]'
+                # [{1}] = Latest Kernel: '[4.12.14-95.54.1]'
+                # {2:.<40} = '.'
+
+                # Needed to check if -default can be replaced always by .1
+                # [{0}] = Installed Kernel: '[4.12.14-94.41-default]'
+
+                print('{3:.>80}\n\nLatest Kernel:{2:.<49}[{1}]\nInstalled Kernel:{2:.<40}[{0}]\n'.format(kernelVersion, latestkenel, '.', '[ FAILED ]'))
+            else:
+                print('Coming soon ! =)')
+                #print('Installed Kernel:{1:.<80}]{1:.<80}{3:<80}'.format(systemInformation['basic_environment'].get('kernelVersion')), '-' , '[ PASSED ]')
+            print('\n\n[Completed!\n')
+    #Debug
+    sto_print()
 
 def main():
 
-    short_options = "ho:f:vep"
-    long_options = ["help", "output=", "file=", "verbose", "extract", "print"]
+    short_options = 'ho:f:vep'
+    long_options = ['help', 'output=', 'file=', 'verbose', 'extract', 'print', 'dir']
 
     try:
         arguments, values = getopt.getopt(argument_list, short_options, long_options)
     except getopt.error as err:
         #output error, and return with an error code
         print(str(err))
-        sys,exit(2)
+        sys, exit(2)
 
     for current_argument, current_value in arguments:
-        if current_argument in ("-v", "--verbose"):
-            print ("Enabling verbose mode")
-        elif current_argument in ("-h", "--help"):
+        if current_argument in ('-v', '--verbose'):
+            print ('Enabling verbose mode')
+        elif current_argument in ('-h', '--help'):
             print_banner()
-            print("Usage: ./proactive_check arguments")
-            print("=============================================================================")
-            print("long argument          short argument            obs               with value")
-            print("-----------------------------------------------------------------------------")
-            print("--help                       -h                                        no")
-            print("--output                     -o                                        yes")
-            print("--help                       -h                                        no")
-            print("--extract                    -e                                        no")
-            print("--print                      -p     ***  Run and print results ***     no")
-            print("-----------------------------------------------------------------------------")
-            print("Example of usage: ./proactive_check -p\n")
-            print("=============================================================================")
-        elif current_argument in ("-e", "--extract"):
+
+            #Nedded to port to Python3
+            middle = '-'*80
+            bottom = '='*80
+
+            print('{:=>80}'.format('='))
+            print('long argument          short argument              obs                with value')
+            print('{:->80}'.format('-'))
+            print('--help                       -h                                               no')
+            print('--output                     -o                                              yes')
+            print('--help                       -h                                               no')
+            print('--extract                    -e                                               no')
+            print('--print                      -p       ***  Run and print results ***          no')
+            print('{:=>80}'.format('-'))
+            print('Example of usage: ./proactive_check -p\n'.format())
+            print('{:=>80}'.format('='))
+        elif current_argument in ('-e', '--extract'):
             extractSupportconfig()
-        elif current_argument in ("-p"):
+        elif current_argument in ('-p'):
             print_formats()
 
 #Uncomment for debug
 #print_formats()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
---
+++ vercheck.py
@@ -0,0 +1,289 @@
+#!/usr/bin/python3
+import re
+import sys
+import urllib3
+import json
+import getopt
+from distutils.version import LooseVersion
+
+# static product list (taken from RMT and other sources)
+product_list = {
+	1117: { 'name': 'SUSE Linux Enterprise Server 12 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12' },
+	1118: { 'name': 'SUSE Linux Enterprise Desktop 12 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12' },
+	1322: { 'name': 'SUSE Linux Enterprise Server 12 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp1' },
+	1333: { 'name': 'SUSE Linux Enterprise Desktop 12 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp1' },
+	1357: { 'name': 'SUSE Linux Enterprise Server 12 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp2' },
+	1358: { 'name': 'SUSE Linux Enterprise Desktop 12 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp2' },
+	1421: { 'name': 'SUSE Linux Enterprise Server 12 SP3 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp3' },
+	1425: { 'name': 'SUSE Linux Enterprise Desktop 12 SP3 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp3' },
+	1625: { 'name': 'SUSE Linux Enterprise Server 12 SP4 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp4' },
+	1629: { 'name': 'SUSE Linux Enterprise Desktop 12 SP4 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:12:sp4' },
+	1878: { 'name': 'SUSE Linux Enterprise Server 12 SP5 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12:sp5' },
+	1319: { 'name': 'SUSE Linux Enterprise Server for SAP 12 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:12' },
+	1346: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp1' },
+	1414: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp2' },
+	1426: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP3 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp3' },
+	1755: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP4 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp4' },
+	1880: { 'name': 'SUSE Linux Enterprise Server for SAP 12 SP5 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:12:sp5' },
+	1575: { 'name': 'SUSE Linux Enterprise Server 15 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:15' },
+	1609: { 'name': 'SUSE Linux Enterprise Desktop 15 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:15' },
+	1763: { 'name': 'SUSE Linux Enterprise Server 15 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:15:sp1' },
+	1764: { 'name': 'SUSE Linux Enterprise Desktop 15 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:15:sp1' },
+	1939: { 'name': 'SUSE Linux Enterprise Server 15 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles:15:sp2' },
+	1935: { 'name': 'SUSE Linux Enterprise Desktop 15 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sled:15:sp2' },
+	1612: { 'name': 'SUSE Linux Enterprise Server for SAP 15 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:15' },
+	1766: { 'name': 'SUSE Linux Enterprise Server for SAP 15 SP1 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:15:sp1' },
+	1941: { 'name': 'SUSE Linux Enterprise Server for SAP 15 SP2 x86_64', 'arch': 'x86_64', 'identifier': 'cpe:/o:suse:sles_sap:15:sp2' },
+}
+	
+# single instance for urllib3 pool
+http = urllib3.PoolManager()
+
+def find_cpe(directory_name):
+	regex = r"CPE_NAME=\"(.*)\""
+	
+	try:
+		f = open(directory_name + '/basic-environment.txt', 'r')
+		text = f.read()
+		f.close()
+
+		matches = re.search(regex, text)
+		for p in product_list:
+			if matches.group(1) == product_list[p]['identifier']:
+				return p
+	except Exception as e:
+		print ('error: ' + str(e))
+	return -1
+
+def read_rpmlist(directory_name):
+	rpmlist = []
+	regex_start = r"(^NAME.*VERSION)\n"
+	regex_package = r"(\S*).*\s([0-9].*)"
+	regex_end = r"(^$)\n"
+	try:
+		f = open(directory_name + '/rpm.txt', 'r')
+		text = f.readlines()
+		f.close()
+
+		found_start = False
+		for line in text:
+			matches = re.search(regex_start, line)
+			if matches != None:
+				found_start=True
+				continue
+			if found_start:
+				matches = re.search(regex_end, line)
+				if matches:
+					break
+
+				matches = re.search(regex_package, line)
+				if matches:	
+					rpmname = matches.group(1)
+					rpmversion = matches.group(2)
+					if rpmname.startswith('gpg-pubkey'):
+						continue
+					if rpmname != '' and rpmversion != '':
+						rpmlist.append([rpmname, rpmversion])
+				else:
+					continue
+	except Exception as e:
+		print('error: ' + str(e))
+
+	return rpmlist
+
+def mySort(e):
+	return LooseVersion(e['version'] + '-' + e['release'])
+
+
+def search_package(product_id, package_name, verbose):
+
+	if verbose is True:
+		print('looking for ' + package_name + ' on product id ' + str(product_id))
+
+	urllib3.disable_warnings()
+
+	try:
+		r = http.request('GET', 'https://scc.suse.com/api/package_search/packages?product_id=' + str(product_id) + '&query=' + package_name, headers={'Accept-Encoding': 'gzip, deflate'})
+	except Exception as e:
+		print('Error while connecting: ' + str(e))
+		exit(1)
+
+	return_data = {}
+
+	if r.status == 200:
+		return_data = json.loads(r.data)
+	elif r.status == 422:
+		json_data = json.loads(r.data)
+		print('cannot be processed due to error: [' + json_data['error'] + ']')
+	elif verbose:
+		print('got error ' + str(r.status) + ' from the server!')
+
+	refined_data = []
+
+	if return_data:
+		for item in return_data['data']:
+			# discard items that do not match exactly our query
+			#print(str(item))
+			if item['name'] != package_name:
+				continue
+			else:
+				#print('products for item: ' + str(item['products']))
+				for product in item['products']:
+					#print(str(product))
+					if verbose:
+						print('version ' + item['version'] + '-' + item['release'] + ' is available on repository [' + product['name'] + ' ' + product['edition'] + ' ' +  product['architecture'] + ']')
+					refined_data.append({'id':item['id'], 'version':item['version'], 'release': item['release']})
+
+	try:	
+		refined_data.sort(reverse=True, key=mySort)
+	except TypeError as e:
+		print('wrong version format: ' + str(e))
+
+	return refined_data
+
+def list_products():
+	print('Known products list')
+	print('ID	Name')
+	print('-----------------------------------------------------')
+	for k, v in product_list.items():
+		print(str(k) + '\t' + v['name'])
+
+	print('total: ' + str(len(product_list)) +  ' products.')
+	return
+
+def usage():
+	print('Usage: ' + sys.argv[0] + ' [-l|--list-products] -p|--product=product id -n|--name <package name> [-s|--short] [-v|--verbose]')
+	return
+
+def test():
+	package_name = 'glibc'
+	for k, v in product_list.items():
+		print('searching for package \"glibc\" in product id \"' + str(k) + '\" (' + v['name'] + ')')
+		refined_data = search_package(k, package_name, True)
+		try:
+			print('latest version for ' + package_name + ' is ' + refined_data[0]['version'] + '-' + refined_data[0]['release'])
+		except IndexError:
+			print('could not find any version for package ' + package_name)
+
+	return
+
+def latestkKernel(v):
+	package_name = 'kernel-default'
+	product_list.get(v)
+	print('searching for latest kernel version:\nhttps://scc.suse.com/api/package_search/packages?product_id={0}'.format(v))
+	refined_data = search_package(v, package_name, False)
+	try:
+		#print('latest version for ' + package_name + ' is ' + refined_data[0]['version'] + '-' + refined_data[0]['release'])
+		lkv = str((refined_data[0]['version'] + '-' + refined_data[0]['release']))
+	except IndexError:
+		print('could not find any version for package ' + package_name)
+
+	return lkv
+
+def check_supportconfig(supportconfigdir):
+
+	uptodate = []
+	notfound = []
+	different = []
+
+	match_os = find_cpe(supportconfigdir)
+	if match_os != -1:
+		print('product name = ' + product_list[match_os]['name'])
+	else:
+		print('error while determining CPE')
+		return ([],[],[])
+
+	rpmlist = read_rpmlist(supportconfigdir)
+	total = len(rpmlist)
+	print('found ' + str(total) + ' total packages to check')
+	
+	count=1
+	for p in rpmlist:
+		refined_data = search_package(match_os, p[0], False)
+		try:
+			latest = refined_data[0]['version'] + '-' + refined_data[1]['release']
+			if latest != p[1]:
+				print('[' + str(count) + '/' + str(total) + '] ' + p[0] + ': latest version is ' + latest + ' (current: ' + p[1] + ')')
+				different.append([p[0], p[1], latest]) 
+			else:
+				print('[' + str(count) + '/' + str(total) + '] ' + p[0] + ': up-to-date')
+				uptodate.append([p[0], p[1]]) 
+
+		except IndexError:
+			print('[' + str(count) + '/' + str(total) + '] ' + p[0] + ': not found')
+			notfound.append(p[0])
+		count+=1
+	return (uptodate, notfound, different)
+
+#### main program
+def main():
+
+	try:
+		opts,args = getopt.getopt(sys.argv[1:],  "hp:n:lsvtd:", [ "help", "product=", "name=", "list-products", "short", "verbose", "test", "supportconfig=" ])
+	except getopt.GetoptError as err:
+		print(err)
+		usage()
+		exit(2)
+
+	product_id = -1
+	package_name = ''
+	short_response = False
+	verbose = False
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+			exit(1)
+		elif o in ("-s", "--short"):
+			short_response = True
+		elif o in ("-p", "--product"):
+			product_id = int(a)
+		elif o in ("-n", "--name"):
+			package_name = a
+		elif o in ("-l", "--list-products"):
+			list_products()
+			exit(0)
+		elif o in ("-d", "--supportconfig"):
+			supportconfigdir = a
+			uptodate, notfound, different = check_supportconfig(supportconfigdir)
+
+			exit(0)
+
+		elif o in ("-v", "--verbose"):
+			verbose = True
+		elif o in ("-t", "--test"):
+			test()
+			exit(0)
+		else:
+			assert False, "invalid option"
+
+
+	if product_id == -1 or package_name is '':
+		print('Please specify a product ID and package name.')
+		usage()
+		exit(2)
+
+	if product_id not in product_list:
+		print ('Product ID ' + str(product_id) + ' is unknown.')
+	else:
+		if verbose is True:
+			print ('Using product ID ' + str(product_id) +  ' ('  + product_list[product_id]['name'] + ')')
+	
+	refined_data = search_package(product_id, package_name, verbose)
+
+	try:
+		if short_response:
+			print(refined_data[0]['version'] + '-' + refined_data[0]['release'])
+		else:
+			print('latest version for ' + package_name + ' is ' + refined_data[0]['version'] + '-' + refined_data[0]['release'])
+	except IndexError:
+		if short_response:
+			print('none')
+		else:
+			print('could not find any version for package ' + package_name)
+		exit(1)
+
+	return
+
+if __name__ == "__main__":
+    main()
+
